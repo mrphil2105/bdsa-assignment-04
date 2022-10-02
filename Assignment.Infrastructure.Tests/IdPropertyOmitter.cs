07af7952dd870abaa2c3a10417298d91d41edd5f
Philip Mørch <mrphil2105@gmail.com>
2022-10-02 16:51:59 +0200

    Create omitter for id properties

---
+++ Assignment.Infrastructure.Tests/IdPropertyOmitter.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using AutoFixture.Kernel;
+
+namespace Assignment.Infrastructure.Tests;
+
+public class IdPropertyOmitter : ISpecimenBuilder
+{
+    public object Create(object request, ISpecimenContext context)
+    {
+        return request switch
+        {
+            // Don't populate id properties, as the database will generate the ids, or they might be foreign keys.
+            PropertyInfo propertyInfo when propertyInfo.Name.EndsWith("Id") => new OmitSpecimen(),
+            // Same with parameters in constructors, but supply a default value, otherwise it will omit the constructor.
+            ParameterInfo parameterInfo when parameterInfo.Name?.EndsWith("Id") ?? false => Activator.CreateInstance(
+                parameterInfo.ParameterType)!,
+            _ => new NoSpecimen()
+        };
+    }
+}
