4a15b7e2688b4b7a0beb40958050ae292d9e2bfd
Philip Mørch <mrphil2105@gmail.com>
2022-10-02 16:52:30 +0200

    Create custom auto data attribute

---
+++ Assignment.Infrastructure.Tests/AutoDbDataAttribute.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+
+namespace Assignment.Infrastructure.Tests;
+
+public class AutoDbDataAttribute : AutoDataAttribute
+{
+    public AutoDbDataAttribute() : base(CreateFixture)
+    {
+    }
+
+    private static IFixture CreateFixture()
+    {
+        var fixture = new Fixture();
+        fixture.Customizations.Add(new IdPropertyOmitter());
+
+        return fixture;
+    }
+}
