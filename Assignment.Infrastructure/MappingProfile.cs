16a561eb24169bd790eb1bb7cf6b29c3cd101a22
Philip Mørch <mrphil2105@gmail.com>
2022-10-02 19:09:05 +0200

    Ignore Created and StateUpdated properties in mapping

--- Assignment.Infrastructure/MappingProfile.cs
+++ Assignment.Infrastructure/MappingProfile.cs
@@ -9,6 +9,8 @@ public class MappingProfile : Profile
         CreateMap<WorkItemCreateDTO, WorkItem>()
             .ForMember(i => i.Id, o => o.Ignore())
             .ForMember(i => i.AssignedTo, o => o.Ignore())
-            .ForMember(i => i.State, o => o.Ignore());
+            .ForMember(i => i.Created, o => o.Ignore())
+            .ForMember(i => i.State, o => o.Ignore())
+            .ForMember(i => i.StateUpdated, o => o.Ignore());
     }
 }
