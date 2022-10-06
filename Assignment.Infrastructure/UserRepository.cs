9388e94dd15dad12a51ba90ec1ef1bb412128263
Philip Mørch <mrphil2105@gmail.com>
2022-10-06 15:12:58 +0200

    Implement method Create

--- Assignment.Infrastructure/UserRepository.cs
+++ Assignment.Infrastructure/UserRepository.cs
@@ -1,5 +1,52 @@
+using AutoMapper;
+
 namespace Assignment.Infrastructure;
 
-public class UserRepository
+public class UserRepository : IUserRepository
 {
+    private readonly KanbanContext _context;
+    private readonly IMapper _mapper;
+
+    public UserRepository(KanbanContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public (Response Response, int UserId) Create(UserCreateDTO user)
+    {
+        var emailExists = _context.Users.Any(u => u.Email == user.Email);
+
+        if (emailExists)
+        {
+            return (Conflict, 0);
+        }
+
+        var entity = _mapper.Map<User>(user);
+
+        _context.Users.Add(entity);
+        _context.SaveChanges();
+
+        return (Created, entity.Id);
+    }
+
+    public UserDTO Find(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IReadOnlyCollection<UserDTO> Read()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Response Update(UserUpdateDTO user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Response Delete(int userId, bool force = false)
+    {
+        throw new NotImplementedException();
+    }
 }
